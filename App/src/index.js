7d66cbf4b7a9589624405a4bf44ba3ca5360f84e
André Aguiar <aguiaroficial@pm.me>
2021-09-25 09:39:19 -0300

    Integração do Middleware

--- App/src/index.js
+++ App/src/index.js
@@ -5,6 +5,8 @@ const bot = new Bot(process.env.TOKEN);
 
 const command_start = require('./commands/start');
 
+const bot_middleware = require('./middlewares/bot');
+
 bot.command("start", command_start.reply);
 
 bot.start();
